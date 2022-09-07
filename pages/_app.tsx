e4c73b1380ecd352f2b3218c975b756334356f53
Murat Güney <muratguney@Murat-MacBook-Pro.local>
2022-09-08 01:16:43 +0300

    zoom is prevented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,37 @@
 import GoogleAnalytics from 'components/GoogleAnalytics';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Provider } from "react-redux";
 import store from 'redux/store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    document.addEventListener(
+      'touchmove',
+      function (event) {
+        if (event['scale'] !== undefined && event['scale'] !== 1) {
+          event.preventDefault();
+        }
+      },
+      { passive: false }
+    );
+    var lastTouchEnd = 0;
+    document.addEventListener(
+      'touchend',
+      function (event) {
+        var now = new Date().getTime();
+        if (now - lastTouchEnd <= 300) {
+          event.preventDefault();
+        }
+        lastTouchEnd = now;
+      },
+      false
+    );
+  }, []);
+
   return (
     <>
       <Provider store={store}>
@@ -13,7 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <title>Lecture Schedule</title>
           <meta
             name="viewport"
-            content={`user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, viewport-fit=cover`}
+            content={`user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width, height=device-height, viewport-fit=cover`}
           />
         </Head>
         <Component {...pageProps} />
