a5313f35baafb027cf23745be22d1374cd355ac0
Murat Güney <muratguney@Murat-MacBook-Pro.local>
2022-09-08 15:16:31 +0300

    db click is removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,8 @@ import Schedule from 'components/Schedule';
 import { NextPage } from 'next/types';
 
 const Home: NextPage = () => {
-    const onDoubleClick = () => {
-        localStorage.removeItem("lectures");
-        localStorage.removeItem("values");
-    }
     return (
-        <div className="min-w-[100vw] min-h-[100vh]" onDoubleClick={onDoubleClick}>
+        <div className="min-w-[100vw] min-h-[100vh]" >
             <Lectures />
             <Schedule />
         </div>
